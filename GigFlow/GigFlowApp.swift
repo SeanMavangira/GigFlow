8f902cead7095096d75f19adc6ff204219e5acbf
Sean <mavangirasean@gmail.com>
2026-03-03 10:43:18 +0200

    Initial Commit

---
+++ GigFlow/GigFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigFlowApp.swift
+//  GigFlow
+//
+//  Created by Sean Mavangira on 3/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct GigFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
