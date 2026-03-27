a718bea2b741cb27e91bb9da0dc14ef264274cfc
SeanMav <mavangirasean@gmail.com>
2026-03-27 10:44:39 +0200

    Created the settings page and added all the pages neccessary to make the settings page to make it function.

---
+++ GigFlow/AllSettings/Currency.swift
@@ -0,0 +1,18 @@
+//
+//  Currency.swift
+//  GigFlow
+//
+//  Created by Sean Mavangira on 27/3/2026.
+//
+
+import SwiftUI
+
+struct Currency: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Currency()
+}
